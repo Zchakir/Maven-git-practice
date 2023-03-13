163560cb8fc3088d2008e21ba8ac021881a16ff0
osameh <osameh@sqli.com>
2023-03-13 15:50:13 +0100

    ImplementMethodsAdd_Size_remove_getHead_getTail

--- Maven-git-practice/src/main/java/LinkedList.java
+++ Maven-git-practice/src/main/java/LinkedList.java
@@ -1,31 +1,50 @@
+import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Objects;
 
 public class LinkedList {
 
     public static class Data{
         public int data;
+
+        public Data(int data) {
+            this.data = data;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Data data1 = (Data) o;
+            return data == data1.data;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(data);
+        }
     }
 
-    private Deque<Data> list;
+    private Deque<Data> list = new ArrayDeque<>() ;
 
     public void addNode(int i) {
-
+        list.add(new Data(i));
     }
 
     public int size() {
-        return 3;
+        return list.size();
     }
 
     public void removeNode(int i) {
-
+        list.remove(new Data(i));
     }
 
 
     public Data getHead() {
-        return null;
+        return list.getFirst();
     }
 
     public Data getTail() {
-        return null;
+        return list.getLast();
     }
 }
