cfc9ccb06724f1aa85021c538ffc4c2a4a85bf85
osameh <osameh@sqli.com>
2023-03-13 15:36:59 +0100

    CreateGetHeadAndGetTailToLinkedList

--- Maven-git-practice/src/main/java/LinkedList.java
+++ Maven-git-practice/src/main/java/LinkedList.java
@@ -1,13 +1,31 @@
+import java.util.Deque;
+
 public class LinkedList {
+
+    public static class Data{
+        public int data;
+    }
+
+    private Deque<Data> list;
+
     public void addNode(int i) {
 
     }
 
     public int size() {
-        return 0;
+        return 3;
     }
 
     public void removeNode(int i) {
 
+    }
+
+
+    public Data getHead() {
+        return null;
+    }
+
+    public Data getTail() {
+        return null;
     }
 }
