4e6228b417204fc8a3f547265aef3dbbf66fbc44
Abdenour Teggour <ateggour@smb.sqli.com>
2023-03-13 15:44:50 +0100

    testRemoveNode validated successfully

--- Maven-git-practice/src/test/java/LinkedList.java
+++ Maven-git-practice/src/test/java/LinkedList.java
@@ -1,15 +1,66 @@
 import java.util.List;
 
 public class LinkedList {
-    List list=new java.util.LinkedList<>();
-    public void addNode(int node){
-        list.add(node);
-    }
-    public void removeNode(int node){
-        list.remove(node);
-    }
-    public int size(){
-        return list.size();
-    }
 
+    Node head;
+
+    public LinkedList(){
+        this.head=null;
+    }
+    public void addNode(Integer data) {
+        Node<Integer> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<Integer> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+    public void removeNode(int value) {
+        if (head == null) {
+            return;
+        }
+
+        // If head node contains the value, set the head to the next node
+        if (head.data==value) {
+            head = head.next;
+            return;
+        }
+
+        // Traverse the list and find the node to remove
+        Node<Integer> current = head;
+        while (current.next != null) {
+            if (current.next.data==value) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
+    }
+    public int size() {
+        int count = 0;
+        Node<Integer> current = this.head;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+    public Node getHead(){
+        return this.head;
+    }
+    public Node<Integer> getTail() {
+        if (head == null) {
+            return null;
+        }
+
+        Node<Integer> current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        return current;
+    }
 }
---
+++ Maven-git-practice/src/test/java/Node.java
@@ -0,0 +1,8 @@
+public class Node<Integer> {
+    public int data;
+    public Node<Integer> next;
+    public Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
