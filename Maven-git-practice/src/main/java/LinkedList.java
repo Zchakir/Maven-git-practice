8f930015da93194697f216307daad7f0937cb2df
Moussa Belfatmi <mbelfatmi@smb.sqli.com>
2023-03-13 15:27:33 +0100

    fixing the first commit

--- Maven-git-practice/src/main/java/LinkedList.java
+++ Maven-git-practice/src/main/java/LinkedList.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class LinkedList {
+public class LinkedList {
+    private Node head;
+    private long size;
+    public LinkedList(){
+        this.size=0L;
+    }
+    public void addNode(Object nodeData){
+        Node node=new Node();
+        node.data=nodeData;
+        node.next=null;
+        if(this.head==null){
+            this.head=node;
+            size++;
+        }else{
+            Node n=head;
+            //looping over the nodes until we find the last one
+            while(n.next!=null) {
+                n=n.next;
+            }
+            n.next=node;
+            size++;
+        }
+
+    }
+    public long size(){
+        return this.size;
+    }
+}
+class Node{
+    public Object data;
+    public Node next;
 }
