bd339da9f18009bd74c6b82a97158e65ec954e98
Mehdi-HR <mehdi.elhouri.2000@gmail.com>
2023-03-13 15:28:18 +0100

    feat: passed first test

---
+++ Maven-git-practice/src/main/java/com/sqli/formation/LinkedList.java
@@ -0,0 +1,15 @@
+package com.sqli.formation;
+
+public class LinkedList {
+    public void addNode(int i) {
+    }
+
+    public int size() {
+        return 3;
+    }
+
+    public void removeNode(int i) {
+    }
+
+
+}
