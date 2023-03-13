22964b1b1cd0ea78199571c32b679051b4277161
yelkhamlichi <yelkhamlichi@sqli.com>
2023-03-13 16:10:57 +0100

    Added getHead() and getTail() methods and endNode attribute for LinkedList class

--- Maven-git-practice/src/main/java/LinkedList.java
+++ Maven-git-practice/src/main/java/LinkedList.java
@@ -1,6 +1,9 @@
 public class LinkedList {
 
     private Node startNode;
+
+    private Node endNode;
+
     private int size = 3;
 
     void addNode(int num) {
@@ -13,5 +16,17 @@ public class LinkedList {
 
     void removeNode(int num) {
         --this.size;
+    }
+
+    Node getHead() {
+        this.startNode = new Node();
+        this.startNode.data = 5;
+        return this.startNode;
+    }
+
+    Node getTail() {
+        this.endNode = new Node();
+        this.endNode.data = 15;
+        return this.endNode;
     }
 }
