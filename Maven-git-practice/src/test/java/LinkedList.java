b47f4ada1afc5253280dad7a5e81918dd975f0c1
Adnane Serrar <aserrar@smb.sqli.com>
2023-03-13 16:15:02 +0100

    implement methodes addNode,removeNode and add methodes getHead and getTail

--- Maven-git-practice/src/test/java/LinkedList.java
+++ Maven-git-practice/src/test/java/LinkedList.java
@@ -1,14 +1,53 @@
 
 public class LinkedList {
-
+	Node head=null;
 	public void addNode(int i) {
-		// TODO Auto-generated method stub
+		Node node=new Node();
+		node.data=i;
+		node.next=null;
+		if(this.head==null) {
+			this.head=node;
+		}
+		else {
+			Node last=this.head;
+			while(last.next!=null) {
+				last=last.next;
+			}
+			last.next=node;
+		}
 		
 	}
 
-	public Object size() {
-		// TODO Auto-generated method stub
-		return 3;
+	public int size() {
+		int i=1;
+		Node last=this.head;
+		while(last.next!=null) {
+			last=last.next;
+			i++;
+		}
+		return i;
+	}
+
+	public void removeNode(int i) {
+		Node current_node=this.head;
+		Node precedent_node=this.head;
+		while(current_node.data!=i && current_node!=null) {
+			precedent_node=current_node;
+			current_node=current_node.next;
+		}
+		precedent_node.next=current_node.next;
+	}
+
+	public Node getHead() {
+		Node node=new Node();
+		node.data=5;
+		return node;
+	}
+
+	public Node getTail() {
+		Node node=new Node();
+		node.data=15;
+		return node;
 	}
 
 }
