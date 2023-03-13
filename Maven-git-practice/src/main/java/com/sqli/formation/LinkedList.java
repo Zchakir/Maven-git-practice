f23025c7288516db0aa5679d6540158beda6590a
Yahya <chahid.yahya@emsi-edu.ma>
2023-03-13 15:13:23 +0100

    "Added addNode"

--- Maven-git-practice/src/main/java/com/sqli/formation/LinkedList.java
+++ Maven-git-practice/src/main/java/com/sqli/formation/LinkedList.java
@@ -1,4 +1,25 @@
 package com.sqli.formation;
 
 public class LinkedList {
+    int size;
+
+    public void addNode(int i) {
+        size++;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public void removeNode(int i) {
+        size --;
+    }
+
+    public Object getHead() {
+        return null;
+    }
+
+    public Object getTail() {
+        return null;
+    }
 }
