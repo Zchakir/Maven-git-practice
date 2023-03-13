78d6188375738166383ff318a89de45476a6f999
Moussa Belfatmi <mbelfatmi@smb.sqli.com>
2023-03-13 15:44:05 +0100

    first test with tdd

--- Maven-git-practice/src/main/java/LinkedList.java
+++ Maven-git-practice/src/main/java/LinkedList.java
@@ -1,32 +1,16 @@
 public class LinkedList {
-    private Node head;
+
     private long size;
     public LinkedList(){
-        this.size=0L;
+        this.size=0;
     }
-    public void addNode(Object nodeData){
-        Node node=new Node();
-        node.data=nodeData;
-        node.next=null;
-        if(this.head==null){
-            this.head=node;
-            size++;
-        }else{
-            Node n=head;
-            //looping over the nodes until we find the last one
-            while(n.next!=null) {
-                n=n.next;
-            }
-            n.next=node;
-            size++;
-        }
-
+    public void addNode(int node){
+        size++;
     }
     public long size(){
         return this.size;
     }
+
 }
 class Node{
-    public Object data;
-    public Node next;
 }
