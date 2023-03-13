05369d151bcbd9903d520d660b47197092072c52
Yousra Terrab <yterrab@smb.sqli.com>
2023-03-13 17:21:00 +0100

    addNode-removeNode-getTail-getHead-size

--- Maven-git-practice/src/main/java/LinkedList.java
+++ Maven-git-practice/src/main/java/LinkedList.java
@@ -1,8 +1,67 @@
 public class LinkedList {
+
+    public class Node {
+        int data;
+        Node next;
+        Node(int d){
+            data = d;
+            next = null;
+        }
+    }
+    public Node head;
+
     public void addNode(int i) {
+        Node new_node = new Node(i);
+        new_node.next = head;
+        head = new_node;
     }
 
     public int size() {
-        return(3);
+        Node temp = head;
+        int count = 0;
+        while (temp != null){
+            count++;
+            temp = temp.next;
+        }
+        return count;
     }
+    public Node getTail() {
+        return this.head;
+    }
+
+   public Node getHead() {
+        Node temp = head;
+        while(temp.next != null) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+    void removeNode(int key)
+    {
+        // Store head node
+        Node temp = head, prev = null;
+
+        // If head node itself holds the key to be deleted
+        if (temp != null && temp.data == key) {
+            head = temp.next; // Changed head
+            return;
+        }
+
+        // Search for the key to be deleted, keep track of
+        // the previous node as we need to change temp.next
+        while (temp != null && temp.data != key) {
+            prev = temp;
+            temp = temp.next;
+        }
+
+        // If key was not present in linked list
+        if (temp == null)
+            return;
+
+        // Unlink the node from linked list
+        prev.next = temp.next;
+    }
+
+
+
 }
