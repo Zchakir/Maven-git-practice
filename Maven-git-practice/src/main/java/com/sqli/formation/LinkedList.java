4a35bb2e84b5e8b8a618c4b281e5bb2ae4afb99b
Salman Hamdouchi <th3salman@gmail.com>
2023-03-13 16:21:11 +0100

    added the Node class and refactored LinkedList so it implements addNode

--- Maven-git-practice/src/main/java/com/sqli/formation/LinkedList.java
+++ Maven-git-practice/src/main/java/com/sqli/formation/LinkedList.java
@@ -1,7 +1,20 @@
 package com.sqli.formation;
 
 public class LinkedList {
+    private Node head;
+
     public void addNode(int i) {
+        Node node = new Node();
+        node.data = i;
+        if(head == null)
+            head = node;
+        else{
+            Node last = head;
+            while(last.next != null){
+                last = last.next;
+            }
+            last.next = node;
+        }
     }
 
     public int size() {
@@ -9,5 +22,12 @@ public class LinkedList {
     }
 
     public void removeNode(int i) {
+    }
+
+    public Node getHead() {
+        return new Node();
+    }
+    public Node getTail() {
+        return new Node();
     }
 }
---
+++ Maven-git-practice/src/main/java/com/sqli/formation/Node.java
@@ -0,0 +1,7 @@
+package com.sqli.formation;
+
+public class Node {
+    public int data;
+    public Node next;
+
+}
