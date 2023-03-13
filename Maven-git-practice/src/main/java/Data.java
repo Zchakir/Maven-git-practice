46ae92d31d3978c86e60d4b49e8ee6b03c348f11
Abdelbarie Ayouni <aayouni@smb.sqli.com>
2023-03-13 16:52:47 +0100

    Done testRemoveNode

---
+++ Maven-git-practice/src/main/java/Data.java
@@ -0,0 +1,11 @@
+public class Data {
+    private int data;
+
+    public Data(int number) {
+        this.data = number;
+    }
+
+    public int getData() {
+        return data;
+    }
+}
--- Maven-git-practice/src/main/java/LinkedList.java
+++ Maven-git-practice/src/main/java/LinkedList.java
@@ -1,8 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class LinkedList {
+    List<Data> dataList;
+
+
+    LinkedList(){
+        dataList=new ArrayList<>();
+    }
+
+
+
+
     public void addNode(int i) {
+
+        this.dataList.add(new Data(i));
     }
 
     public int size() {
-        return 3;
+       /* int number=0;
+        while (this.dataList.get(number)!=null)
+            number++;*/
+        return this.dataList.size();
+    }
+
+    public void removeNode(int i) {
+        this.dataList.remove(1);
+    }
+
+    public Data getHead() {
+
+        return this.dataList.get(0);
+    }
+
+    public Data getTail() {;
+        return  this.dataList.get(dataList.size()-1);
     }
 }
